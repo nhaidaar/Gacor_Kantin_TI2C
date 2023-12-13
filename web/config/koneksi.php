a4fa3fff0ea413aacd232aed1217ed8d3822a652
akfalah <akfalah1107@gmail.com>
2023-12-13 23:52:04 +0700

    .

--- web/config/koneksi.php
+++ web/config/koneksi.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("Asia/Jakarta");
-$koneksi = mysqli_connect("localhost", "root", "", "kantin");
-
+// $koneksi = mysqli_connect("localhost", "root", "", "kantin");
+$koneksi = mysqli_connect("localhost", "akfalah", "2905", "dbkantin");
 
 if (mysqli_connect_errno()) {
     die("Koneksi database gagal: " . mysqli_connect_error());
--- web/module/transaction/index.php
+++ web/module/transaction/index.php
@@ -1,207 +1,109 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Kantin Gacor</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="assets/logo-jti.png" type="image/x-icon">
-</head>
-
-<body>
-    <div class="sidebar">
-        <div class="jti">
-            <img src="assets/logo-jti.png">
-            <div class="title">JTI Polinema</div>
-        </div>
-        <ul>
-            <li>
-                <a href="index.php?page=dashboard">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.458 8.125H4.167A1.667 1.667 0 0 1 2.5 6.458V4.167c0-.921.746-1.667 1.667-1.667h2.291c.921 0 1.667.746 1.667 1.667v2.291c0 .921-.746 1.667-1.667 1.667Zm9.375 0h-2.291a1.667 1.667 0 0 1-1.667-1.667V4.167c0-.921.746-1.667 1.667-1.667h2.291c.921 0 1.667.746 1.667 1.667v2.291c0 .921-.746 1.667-1.667 1.667ZM6.458 17.5H4.167A1.667 1.667 0 0 1 2.5 15.833v-2.291c0-.921.746-1.667 1.667-1.667h2.291c.921 0 1.667.746 1.667 1.667v2.291c0 .921-.746 1.667-1.667 1.667Zm9.375 0h-2.291a1.667 1.667 0 0 1-1.667-1.667v-2.291c0-.921.746-1.667 1.667-1.667h2.291c.921 0 1.667.746 1.667 1.667v2.291c0 .921-.746 1.667-1.667 1.667Z" clip-rule="evenodd" />
-                    </svg>
-                    Dashboard</a>
-            </li>
-            <li>
-                <a href="index.php?page=product">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12.501 7.186V7.5a2.501 2.501 0 0 1-2.5 2.501v0A2.501 2.501 0 0 1 7.498 7.5v-.313" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12.501 7.186V7.5a2.501 2.501 0 0 0 2.501 2.5h.207a2.294 2.294 0 0 0 2.294-2.294v0c0-.34-.092-.674-.267-.965l-2-3.333a1.876 1.876 0 0 0-1.609-.911H6.373c-.66 0-1.27.346-1.609.91l-2 3.334a1.876 1.876 0 0 0-.267.965v0A2.294 2.294 0 0 0 4.79 10h.207a2.501 2.501 0 0 0 2.5-2.501v-.313" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16.565 9.554v6.073c0 1.036-.84 1.876-1.876 1.876H5.312a1.876 1.876 0 0 1-1.876-1.876V9.553m7.971 4.485H8.593" />
-                    </svg>
-                    Product</a>
-            </li>
-            <li>
-                <a href="index.php?page=transaction" class="active">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1.5" d="M18.333 5v2.017c0 1.316-.833 2.15-2.15 2.15h-2.85V3.342c0-.925.759-1.675 1.684-1.675a3.35 3.35 0 0 1 2.341.975c.6.608.975 1.441.975 2.358Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1.5" d="M1.667 5.833V17.5A.83.83 0 0 0 3 18.167L4.425 17.1a.84.84 0 0 1 1.1.083l1.383 1.392a.84.84 0 0 0 1.184 0l1.4-1.4a.826.826 0 0 1 1.083-.075L12 18.167a.835.835 0 0 0 1.333-.667V3.333c0-.916.75-1.666 1.667-1.666H5C2.5 1.667 1.667 3.158 1.667 5v.833Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7.5 10.842H10M7.5 7.508H10" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.996 10.833h.008M4.996 7.5h.008" />
-                    </svg>
-                    Transaction</a>
-            </li>
-            <li>
-                <a href="index.php?page=notification">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
-                        <path stroke="#1B1B1B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.995 10.833V7.5c0-2.762 2.24-5 5.005-5a5.002 5.002 0 0 1 5.005 5v3.333c0 .822.373 1.6 1.014 2.114l.258.207c.764.614.33 1.846-.65 1.846H4.373c-.98 0-1.414-1.232-.65-1.846l.258-.207a2.71 2.71 0 0 0 1.014-2.114Z" clip-rule="evenodd" />
-                        <path stroke="#1B1B1B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M8.75 17.5h2.5" />
-                    </svg>
-                    Notification
-                    <div class="notif">99+</div>
-                </a>
-            </li>
-        </ul>
-        <div class="logout">
-            <a href="logout.php">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
-                    <path stroke="#EC1A1A" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9.167 16.16V6.252c0-.575-.296-1.109-.784-1.413L5.05 2.756C3.94 2.062 2.5 2.86 2.5 4.169v9.907c0 .575.296 1.109.783 1.413l3.334 2.084c1.11.694 2.55-.105 2.55-1.413Z" clip-rule="evenodd" />
-                    <path stroke="#EC1A1A" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12.5 9.167h5m-1.667 1.666L17.5 9.167 15.833 7.5m-6.666 8.333H12.5c.92 0 1.667-.745 1.667-1.666v-.834m0-8.333v-.833c0-.921-.746-1.667-1.667-1.667H4.167" />
-                </svg>
-                Log Out</a>
+<div class="content">
+    <div class="header-fixed">
+        <div class="htitle">
+            Transaction
         </div>
     </div>
-
-    <div class="content">
-        <div class="header-fixed">
-            <div class="htitle">
-                Transaction
-            </div>
+    <div class="header">
+        <div class="htitle">
+            Transaction
         </div>
-        <div class="header">
-            <div class="htitle">
-                Transaction
-            </div>
-        </div>
-        
-        <div class="container">
-            <div class="product-row">
-                <div class="searchbar">
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="9.21552" cy="9.21552" r="5.88495" stroke="#1B1B1B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M16.6695 16.6695L13.3765 13.3765" stroke="#1B1B1B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    </div>
+    <div class="container">
+        <div class="product-row">
+            <div></div>
+            <div style="gap: 8px; display: flex;">
+                <div onclick="openModal()" class="edit-product" id="filter">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
+                        <path stroke="#1B1B1B" stroke-linecap="round" stroke-width="1.5" d="M2.5 5.833h15M5 10h10m-6.667 4.167h3.334" />
                     </svg>
-                    <input type="text" name="search" class="searchbox" placeholder="Search">
+                    Filter
                 </div>
-                <div style="gap: 8px; display: flex;">
-                    <div class="edit-product">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
-                            <path stroke="#1B1B1B" stroke-linecap="round" stroke-width="1.5" d="M2.5 5.833h15M5 10h10m-6.667 4.167h3.334" />
-                        </svg>
-                        Filter
-                    </div>
+                <a href="index.php?page=transaction/add_transaction/">
                     <div class="edit-product-yellow">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
                             <path stroke="#1B1B1B" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5 10h10m-5 5V5" />
                         </svg>
                         Add Transaction
                     </div>
-                </div>
-            </div>
-            <div style="height: 32px;"></div>
-
-            <table class="transaction">
-                <thead>
-                    <tr>
-                        <th>Order ID</th>
-                        <th>Product</th>
-                        <th>Time</th>
-                        <th>Date</th>
-                        <th>Total Price</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $query = "SELECT 
-                                    t.id AS order_id,
-                                    GROUP_CONCAT(CONCAT(ti.qty, 'pcs ', p.product_name) SEPARATOR '\n') AS product,
-                                    TIME(t.date) AS transaction_time,
-                                    DATE(t.date) AS transaction_date,
-                                    SUM(ti.total_price) AS total_price
-                                FROM 
-                                    transactions t
-                                INNER JOIN 
-                                    transaction_items ti ON t.id = ti.transactions_id
-                                INNER JOIN 
-                                    product p ON ti.product_id = p.id
-                                GROUP BY 
-                                    t.id
-                                ORDER BY t.date DESC
-                                ";
-                    $result = mysqli_query($koneksi, $query);
-                    while ($row = mysqli_fetch_assoc($result)) {
-                    ?>
-                        <tr>
-                            <td><?= $row['order_id']; ?></td>
-                            <td><?= $row['product']; ?></td>
-                            <td><?= $row['transaction_time']; ?></td>
-                            <td><?= date("d-m-Y", strtotime($row['transaction_date'])); ?></td>
-                            <td>IDR <?= number_format($row['total_price'], 2, ',', '.'); ?></td>
-                            <td><a id="<?= $row['order_id']; ?>" onclick="openModal()">View Detail</a></td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-    <div id="modal" class="modal-transaction">
-        <div class="modal-container">
-            <div class="modal-header">
-                Detail Transaction
-            </div>
-            <div class="modal-content">
-                <div class="product-row">
-                    <div class="order-attribute">
-                        Order ID
-                    </div>
-                    #00001234
-                </div>
-                <div class="product-row">
-                    <div class="order-attribute">
-                        Time
-                    </div>
-                    #00001234
-                </div>
-                <div class="product-row">
-                    <div class="order-attribute">
-                        Date
-                    </div>
-                    #00001234
-                </div>
-                <table class="tx-detail">
-                    <thead>
-                        <tr>
-                            <th>Product</th>
-                            <th style="text-align: end; color: #1B1B1B;">4 Items</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Teh Pucuk</td>
-                            <td style="text-align: end;">IDR 2.500,00</td>
-                        </tr>
-                    </tbody>
-                </table>
-
-            </div>
-            <div class="modal-content" style="border-top: 2px #EBEBEB dashed;">
-                <div class="product-row">
-                    <div class="order-attribute">
-                        Total Price
-                    </div>
-                    #00001234
-                </div>
-            </div>
-            <div class="modal-footer">
-                <div id="close-modal" class="modal-button" onclick="closeModal()">Close</div>
+                </a>
             </div>
         </div>
+        <div style="height: 32px;"></div>
+
+        <table class="transaction">
+            <thead>
+                <tr>
+                    <th>Order ID</th>
+                    <th>Product</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Total Price</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                require 'class/transaction.php';
+                $transaction = new Transaction($koneksi);
+
+                if (isset($_GET['datestart']) && isset($_GET['dateend'])) {
+                    $transaction->fetchTransactionByDate($_GET['datestart'], $_GET['dateend']);
+                } else {
+                    $transaction->fetchAllTransaction(null);
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
+</div>
 
+<div id="modal" class="modal-transaction">
+    <div class="modal-container">
+        <div class="modal-header">
+            Filter by Date
+        </div>
+        <div class="modal-content" style="display: flex; gap: 12px;">
+            <div class="myform">
+                <label for="datestart">Date start :</label>
+                <input type="date" name="datestart" id="datestart" style="border-radius: 8px; padding: 16px; border: 1px #E1E1E1 solid;">
+            </div>
+            <div class="myform">
+                <label for="dateend">Date end :</label>
+                <input type="date" name="dateend" id="dateend" style="border-radius: 8px; padding: 16px; border: 1px #E1E1E1 solid;">
+            </div>
+        </div>
+        <div class=" modal-footer" style="display: flex; gap: 14px;">
+            <div onclick="closeModal()" class="request-stock">Close</div>
+            <div id="submit" class="request-stock" style="background-color: #FFC300;">Submit</div>
+        </div>
+    </div>
+</div>
+<script src="script.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#submit').click(function(e) {
+            e.preventDefault();
+            var datestart = $('#datestart').val();
+            var dateend = $('#dateend').val();
 
-    
-    <script src="script.js"></script>
+            // make the date is plus 1
+            var nextDay = new Date(dateend);
+            nextDay.setDate(nextDay.getDate() + 1);
+
+            var formattedDateEnd = nextDay.toISOString().split('T')[0]; // Format as YYYY-MM-DD
+            var url = "index.php?page=transaction&datestart=" + encodeURIComponent(datestart) + "&dateend=" + encodeURIComponent(formattedDateEnd);
+
+            $.ajax({
+                type: "GET",
+                url: url,
+                success: function(response) {
+                    window.location.href = url;
+                },
+            });
+        });
+    });
+</script>
 </body>
 
 </html>
